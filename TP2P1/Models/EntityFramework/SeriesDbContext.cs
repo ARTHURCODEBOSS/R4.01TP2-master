35b68f9c6ed7fce474e500d6257ecf8f39a1f02a
NielsGrrr <niels.stiefvater@etu.univ-smb.fr>
2026-02-03 14:46:58 +0100

    alwaysdata

--- TP2P1/Models/EntityFramework/SeriesDbContext.cs
+++ TP2P1/Models/EntityFramework/SeriesDbContext.cs
@@ -19,7 +19,7 @@ public partial class SeriesDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Server=51.83.36.122;port=5432;Database=madata; uid=s314-clubmed; password=jaimepaslespieds");
+        => optionsBuilder.UseNpgsql("Server=postgresql-lopesgames.alwaysdata.net;port=5432;Database=lopesgames_bd; uid=lopesgames; password=APqmWN1092_TgV");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
